642effe6872bdd761ef09da922624ab966be3fd4
Matthew Rocklin <mrocklin@gmail.com>
2013-04-18 07:48:58 -0500

    add matrices directory and core file

---
+++ computations/matrices/core.py
@@ -0,0 +1,101 @@
+from computations import Computation
+from computations.util import unique
+from sympy import Symbol, Expr, Basic, ask, Tuple, S
+from sympy.matrices.expressions import MatrixExpr, MatMul, ZeroMatrix
+from sympy.strategies.tools import subs
+from strategies import exhaust, chain
+from sympy.strategies.traverse import bottom_up
+
+def is_number(x):
+    """ Is either a Python number or a SymPy number """
+    return (isinstance(x, (int, float)) or
+            isinstance(x, Expr) and x.is_Number)
+
+def remove_numbers(coll):
+    """ Remove numbers from a collection
+
+    >>> from sympy.computations.matrices.core import remove_numbers
+    >>> remove_numbers([1, 'x', 5, 'y'])
+    ['x', 'y']
+    """
+    return filter(lambda x: not is_number(x), coll)
+
+basetypes = {'S': 'real*4',
+             'D': 'real*8',
+             'C': 'complex*8',
+             'Z': 'complex*16'}
+
+def defloat(x):
+    return S(x) if isinstance(x, (int, float)) else x
+
+class MatrixCall(Computation):
+    """ An atomic call, superclass for BLAS and LAPACK """
+    def __init__(self, *args):
+        if args[-1] not in basetypes:
+            typecode = 'D'
+        else:
+            args, typecode = args[:-1], args[-1]
+        self.args = tuple(map(defloat, args))
+        self.typecode = typecode
+
+    inputs = property(lambda self: self.args)
+   # inputs = property(lambda self:
+   #                 tuple(map(exhaust(bottom_up(canonicalize)),
+   #                           unique(remove_numbers(self.all_inputs)))))
+
+    @property
+    def outputs(self):
+        cls = self.__class__
+        mapping = dict(zip(cls._inputs, self.inputs))
+        return tuple(canonicalize(o.xreplace(mapping)) for o in cls._outputs)
+
+    basetype = property(lambda self:  basetypes[self.typecode])
+    _in_types = property(lambda self: (None,)*len(self._inputs))
+    _out_types = property(lambda self: (None,)*len(self._outputs))
+
+    in_types  = property(lambda self:
+                          tuple(it or self.basetype for it in self._in_types))
+    out_types = property(lambda self:
+                          tuple(ot or self.basetype for ot in self._out_types))
+
+    @classmethod
+    def valid(cls, inputs, assumptions=True):
+        d = dict(zip(cls._inputs, inputs))
+        if cls.condition is True:
+            return True
+        return ask(cls.condition.xreplace(d), assumptions)
+
+    @classmethod
+    def fnname(cls, typecode):
+        return (typecode+cls.__name__).lower()
+
+    @staticmethod
+    def arguments(inputs, outputs):
+        return inputs
+
+    def fortran_call(self, input_names, output_names):
+        args = type(self).arguments(self.inputs, self.outputs)
+        name_map = dict(zip(self.inputs+self.outputs, input_names+output_names))
+        argnames = [a if is_number(a) else name_map[a] for a in args]
+        codemap = self.codemap(argnames)
+        return self.fortran_template % codemap
+
+def nameof(var):
+    """ Fortran name of variable """
+    if is_number(var.expr):
+        return var.expr
+    else:
+        return var.token
+
+def canonicalize(x):
+    if isinstance(x, MatrixExpr):
+        return x.doit()
+    if isinstance(x, Symbol):
+        return x
+    if isinstance(x, Expr):
+        try:
+            return type(x)(*x.args)
+        except:
+            return x
+    else:
+        return x
---
+++ computations/matrices/tests/test_core.py
@@ -0,0 +1,26 @@
+from computations.matrices.core import remove_numbers, canonicalize
+from computations.matrices.blas import GEMM
+from sympy.matrices.expressions import MatrixSymbol, MatAdd
+from sympy.core import Symbol, S, Float
+
+def test_remove_numbers():
+    X = MatrixSymbol('X', 1, 3)
+    x = Symbol('x')
+    assert remove_numbers([x, X, 1, 1.0, S.One]) == [x, X]
+
+def test_inplace():
+    a = Symbol('a')
+    X = MatrixSymbol('X', 3, 3)
+    Y = MatrixSymbol('Y', 3, 3)
+    g = GEMM(a, X, Y, S.Zero, Y)
+    assert g.inplace == {0: 4}
+
+def test_canonicalize():
+    X = MatrixSymbol('X', 3, 3)
+    assert canonicalize(MatAdd(X, X)) == 2*X
+
+def test_sympify_floats():
+    X = MatrixSymbol('X', 3, 3)
+    Y = MatrixSymbol('Y', 3, 3)
+    g = GEMM(2.0, X, Y, 0.0, Y)
+    assert isinstance(g.args[0], Float)
