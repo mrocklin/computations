73ecec86facf06d6e735b1223fdcba64970871e7
Matthew Rocklin <mrocklin@gmail.com>
2013-05-02 11:40:23 -0500

    dot only produces interesting vars

--- computations/dot/core.py
+++ computations/dot/core.py
@@ -1,4 +1,12 @@
 
+def interesting_var(x):
+    if any(x == y for y in [0, 1]):
+        return False
+    return True
+
+def interesting_edge((a, b)):
+    return all(map(interesting_var, (a, b)))
+
 def declare_var(variable):
     if hasattr(variable, '_write_dot'):
         return variable._write_dot()
@@ -10,8 +18,9 @@ def declare_comp(comp):
     return '"%s" [shape=box, label="%s"]'%(nstr(comp), nstr(comp.__class__.__name__))
 
 def call(comp):
-    return '\n'.join(['"%s" -> "%s"' % tuple(map(nstr, edge)) for edge in
-        comp.edges()])
+    return '\n'.join(['"%s" -> "%s"' % tuple(map(nstr, edge))
+                        for edge in comp.edges()
+                        if interesting_edge(edge)])
 
 def nstr(o):
     """ Normalized str """
@@ -37,10 +46,11 @@ def generate_dot(computation, **kwargs):
     """ Generate DOT string for computation """
     flags = defaults.copy(); flags.update(kwargs)
     flags = '\n'.join(['%s=%s'%item for item in flags.items()])
-    variables = '\n'.join(map(declare_var, computation.variables))
+    variables = '\n'.join(map(declare_var,
+                              filter(interesting_var, computation.variables)))
     computations = '\n'.join(map(declare_comp, computation.toposort()))
     calls = '\n'.join(map(call, computation.toposort()))
-    return template%locals()
+    return template % locals()
 
 def writepdf(computation, filename, extension='pdf', **kwargs):
     import os
