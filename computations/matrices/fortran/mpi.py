2a3948756a51ffa7d449e271df04f7965575d0bf
Matthew Rocklin <mrocklin@gmail.com>
2013-06-05 17:50:15 -0500

    rank_switch routine

--- computations/matrices/fortran/mpi.py
+++ computations/matrices/fortran/mpi.py
@@ -12,4 +12,24 @@ def generate_mpi_tester(comp, *args, **kwargs):
 
     return mpi_test_template % locals()
 
+template = """
+subroutine rank_switch()
+    implicit none
+    include 'mpif.h'
 
+    integer rank, size, ierr
+
+    call MPI_COMM_RANK( MPI_COMM_WORLD, rank, ierr )
+    call MPI_COMM_SIZE( MPI_COMM_WORLD, size, ierr )
+
+    if (size .lt. %(max_rank)s)  print *, 'Need %(max_rank)s processes'
+
+    %(switch)s
+end subroutine rank_switch"""
+
+def rank_switch(d):
+    """ Fortran code to switch subroutine based on MPI rank """
+    max_rank = max(d.keys()) + 1
+    switch = '\n    '.join('if (rank .eq. %(rank)d)  %(fn)s()'%{'rank': k, 'fn': v}
+            for k, v in d.items())
+    return template % locals()
--- computations/matrices/fortran/tests/test_mpi.py
+++ computations/matrices/fortran/tests/test_mpi.py
@@ -1,5 +1,6 @@
 from sympy import MatrixSymbol, Symbol, Q, assuming
 from computations.inplace import inplace_compile
+from computations.matrices.fortran.mpi import rank_switch
 
 def test_generate_mpi_program():
     from computations.matrices.io import ReadFromFile, WriteToFile
@@ -10,3 +11,11 @@ def test_generate_mpi_program():
     with assuming(Q.real_elements(A)):
         s = generate_mpi_tester(icomp, [], [])
     assert isinstance(s, str)
+
+def test_rank_switch():
+    d = {0: 'A', 1: 'B', 2: 'C'}
+    s = rank_switch(d)
+    assert "if (rank .eq. 0)  A()" in s
+    assert "if (rank .eq. 1)  B()" in s
+    assert "if (rank .eq. 2)  C()" in s
+    assert "Need 3 processes" in s
