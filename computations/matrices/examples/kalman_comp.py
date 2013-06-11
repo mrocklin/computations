bfe234e1e451b293f8956622e70d11c525a268aa
Matthew Rocklin <mrocklin@gmail.com>
2013-06-11 09:48:38 -0500

    kalman_comp defines function rather than explicit computation

--- computations/matrices/examples/kalman_comp.py
+++ computations/matrices/examples/kalman_comp.py
@@ -8,21 +8,25 @@ from sympy.matrices.expressions import ZeroMatrix, Transpose
 from sympy.matrices.expressions.factorizations import UofCholesky
 from sympy import assuming, Q
 
-Z = H*Transpose(H*Sigma) + R
-A = Z.I * (-1.0*data + H*mu)
-B = Z.I * H
+def make_kalman_comp(mu, Sigma, H, R, data, assumptions=assumptions):
+    Z = H*Transpose(H*Sigma) + R
+    A = Z.I * (-1.0*data + H*mu)
+    B = Z.I * H
 
-with assuming(*assumptions):
-    c = (
-         SYMM(1.0, H, Sigma, 0.0, ZeroMatrix(*H.shape)) +
-         GEMM(1.0, H, mu, -1.0, data) +
-         GEMM(1.0, H, Transpose(H*Sigma), 1.0, R) +
-         POSV(Z, H) +
-         POTRS(UofCholesky(Z), -1.0 * data + H*mu) +
+    with assuming(*assumptions):
+        c = (
+             SYMM(1.0, H, Sigma, 0.0, ZeroMatrix(*H.shape)) +
+             GEMM(1.0, H, mu, -1.0, data) +
+             GEMM(1.0, H, Transpose(H*Sigma), 1.0, R) +
+             POSV(Z, H) +
+             POTRS(UofCholesky(Z), -1.0 * data + H*mu) +
 
-         GEMM(1.0, H.T, A, 0.0, ZeroMatrix(*(H.T*A).shape)) +
-         SYMM(1.0, Sigma, H.T*A, 1.0, mu) +
+             GEMM(1.0, H.T, A, 0.0, ZeroMatrix(*(H.T*A).shape)) +
+             SYMM(1.0, Sigma, H.T*A, 1.0, mu) +
 
-         GEMM(1.0, H.T, B, 0.0, ZeroMatrix(*(H.T*B).shape)) +
-         SYMM(1.0, H.T*B, Sigma, 0.0, ZeroMatrix(*Sigma.shape)) +
-         SYMM(-1.0, Sigma, H.T*B*Sigma, 1.0, Sigma))  # careful, overwrite
+             GEMM(1.0, H.T, B, 0.0, ZeroMatrix(*(H.T*B).shape)) +
+             SYMM(1.0, H.T*B, Sigma, 0.0, ZeroMatrix(*Sigma.shape)) +
+             SYMM(-1.0, Sigma, H.T*B*Sigma, 1.0, Sigma))  # careful, overwrite
+    return c
+
+c = make_kalman_comp(mu, Sigma, H, R, data, assumptions)
