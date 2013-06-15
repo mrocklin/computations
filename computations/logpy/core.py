8e9663d9618694be3fee12f90a7910d86e78c275
Matthew Rocklin <mrocklin@gmail.com>
2013-06-15 17:02:02 -0500

    _as/from_tuple -> _as/from_logpy

--- computations/logpy/core.py
+++ computations/logpy/core.py
@@ -1,12 +1,12 @@
 from computations.core import Computation, CompositeComputation, Identity
 
-Computation._as_tuple = lambda self: (type(self), self.inputs, self.outputs)
-Identity._as_tuple = lambda self: (type(self), self.inputs)
-CompositeComputation._as_tuple = lambda self: (type(self),) + self.computations
+Computation._as_logpy = lambda self: (type(self), self.inputs, self.outputs)
+Identity._as_logpy = lambda self: (type(self), self.inputs)
+CompositeComputation._as_logpy = lambda self: (type(self), self.computations)
 
-Computation._from_tuple = staticmethod(lambda (t, ins, outs): t(ins, outs))
-Identity._from_tuple = staticmethod(lambda (t, ins): t(ins))
-CompositeComputation.from_tuple = staticmethod(lambda tup: tup[0](*tup[1:]))
+Computation._from_logpy = staticmethod(lambda (t, ins, outs): t(ins, outs))
+Identity._from_logpy = staticmethod(lambda (t, ins): t(ins))
+CompositeComputation._from_logpy = staticmethod(lambda (op, args): op(*args))
 
 from logpy import fact
 from logpy.assoccomm import commutative, associative
--- computations/matrices/logpy/core.py
+++ computations/matrices/logpy/core.py
@@ -1,5 +1,5 @@
 from computations.matrices.core import MatrixCall
 import computations.logpy
 
-MatrixCall._as_tuple = lambda self: (type(self), self.args, self.typecode)
-MatrixCall._from_tuple = staticmethod(lambda (t, args, tc): t(*(args+(tc,))))
+MatrixCall._as_logpy = lambda self: (type(self), self.args, self.typecode)
+MatrixCall._from_logpy = staticmethod(lambda (t, args, tc): t(*(args+(tc,))))
