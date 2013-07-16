bab10052360a45c1517473481bed368a3df5dbfd
Matthew Rocklin <mrocklin@gmail.com>
2013-07-15 22:05:07 -0500

    squashme

--- computations/matrices/tests/test_shared.py
+++ computations/matrices/tests/test_shared.py
@@ -1,7 +1,14 @@
-from computations.matrices.shared import detranspose
+from computations.matrices.shared import detranspose, uplo
 from sympy.matrices.expressions import MatrixSymbol
 
 def test_detranspose():
     X = MatrixSymbol('X', 2, 3)
     assert detranspose(X) == X
     assert detranspose(X.T) == X
+
+def test_side():
+    try:
+        uplo(X, [])
+        assert False
+    except Exception:
+        assert True
